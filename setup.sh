57bcc0b3dd2252215f53449410e9bc1883cec2ac
Nikolay Kondakov <k2n@yandex.ru>
2023-04-14 15:21:06 +0300

    Update setup.sh
    Changed time zone

--- setup.sh
+++ setup.sh
@@ -92,7 +92,7 @@ function setupTimezone() {
     echo -ne "Enter the timezone for the server (Default is 'Asia/Singapore'):\n" >&3
     read -r timezone
     if [ -z "${timezone}" ]; then
-        timezone="Asia/Singapore"
+        timezone="Europe/Moscow"
     fi
     setTimezone "${timezone}"
     echo "Timezone is set to $(cat /etc/timezone)" >&3
