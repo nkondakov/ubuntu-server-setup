40e08c085abb485bab8b015ef9109fcb05cf30bd
Nikolay Kondakov <k2n@yandex.ru>
2023-04-14 16:17:09 +0300

    Update setupLibrary.sh

--- setupLibrary.sh
+++ setupLibrary.sh
@@ -175,7 +175,7 @@ function installDocker() {
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
     sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
-    sudo groupadd docker
+    sudo groupadd -f docker
     sudo usermod -aG docker $USER
 }
 
